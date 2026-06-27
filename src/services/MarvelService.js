423b1c52c3966dfdecc4af7e873e3e384c3fe72a
Klimenko Danil <Crazy_1099@mail.ru>
2026-06-27 13:09:19 +0300

    add lesson22

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,6 @@
 class MarvelService {
-    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
-    _apiKey = 'apikey=c5d6fc8b83116d92ed468ce36bac6c62';
+    _apiBase = 'https://marvel-server-zeta.vercel.app/';
+    _apiKey = 'd4eecb0c66dedbfae4eab45d312fc1df';
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -14,22 +13,24 @@ class MarvelService {
     }
 
     getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters?apikey=${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
     getCharacter = async (id) => {
-        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters/${id}?apikey=${this._apiKey}`);
         return this._transformCharacter(res.data.results[0]);
     }
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
